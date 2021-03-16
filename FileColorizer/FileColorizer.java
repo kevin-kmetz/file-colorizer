a4dfaeea6eb9caaa38b300fa1dfdfd8e39765186
Kevin Kmetz <kev.kmetz@gmail.com>
2021-03-16 00:29:32 -0700

    Colorizer can now determine number of pixels needed in the image, and determines the image width if none is provided.

--- FileColorizer/FileColorizer.java
+++ FileColorizer/FileColorizer.java
@@ -4,18 +4,51 @@ import java.io.File;
 
 public class FileColorizer {
 
+	int imageWidth;
+	int imageHeight;
+
+	int numberOfFullPixels;
+	int leftOverBytes;
+	int totalPixels;
+
 	public static void main(String[] args) {
 
 		FileColorizer colorizer = new FileColorizer(args[0]);
 
 	}
 
-
 	FileColorizer(String fileName) {
 
-		File file = new File(fileName);
+		this(fileName, -1);
 
-		System.out.println("File size: " + file.length());
+	}
+
+
+	FileColorizer(String fileName, int imageWidth) {
+
+		File file = new File(fileName);
+		int fileSize = Math.toIntExact(file.length());
+
+		numberOfFullPixels = fileSize / 3;
+		leftOverBytes = fileSize % 3;
+
+		totalPixels = numberOfFullPixels;
+
+		if (leftOverBytes > 0) {
+
+			totalPixels++;
+
+		}
+
+		this.imageWidth = imageWidth;
+
+		if (imageWidth <= 0) {
+
+			this.imageWidth = (int) Math.ceil(Math.sqrt((double) totalPixels));
+
+		}
+
+		System.out.println(this.imageWidth);
 
 	}
 
