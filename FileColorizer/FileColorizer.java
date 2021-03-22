cf9910ba3a782b8056c0e6bf04c46e9beea0265f
Kevin Kmetz <kev.kmetz@gmail.com>
2021-03-21 20:08:28 -0700

    Added a way to calculate the number of rows needed in the output image. Also verified that calculations are correct.

--- FileColorizer/FileColorizer.java
+++ FileColorizer/FileColorizer.java
@@ -7,13 +7,21 @@ public class FileColorizer {
 	int imageWidth;
 	int imageHeight;
 
-	int numberOfFullPixels;
-	int leftOverBytes;
-	int totalPixels;
+	int numberOfFullPixels;		// Number of pixels that won't need any padding in RGB.
+	int leftOverBytes;			// Number of bytes needed to make final pixel complete (0, 1, or 2).
+	int totalPixels;			// Number of pixels, including extra bytes needed to get a full final pixel.
 
 	public static void main(String[] args) {
 
-		FileColorizer colorizer = new FileColorizer(args[0]);
+		if (args.length == 1) {
+
+			FileColorizer colorizer = new FileColorizer(args[0]);
+
+		} else if (args.length == 2) {
+
+			FileColorizer colorizer = new FileColorizer(args[0], Integer.parseInt(args[1]));
+
+		}
 
 	}
 
@@ -48,7 +56,15 @@ public class FileColorizer {
 
 		}
 
+		imageHeight = totalPixels / this.imageWidth;
+
+		if (totalPixels % this.imageWidth != 0) {
+			imageHeight++;
+		}
+
 		System.out.println(this.imageWidth);
+		System.out.println(imageHeight);
+		System.out.println(totalPixels);
 
 	}
 
